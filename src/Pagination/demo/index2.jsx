67d2217fd61279ae1b58c4a93eadbda845cddef0
Found-404 <2247124474@qq.com>
2022-11-22 17:49:38 +0800

    fix(all): 修复查看代码会显示所有关联文件

--- src/Pagination/demo/index2.jsx
+++ src/Pagination/demo/index2.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Paginaction from '..'
+import React from 'react';
+import { Paginaction } from 'react-view-design';
 export default function index() {
   return (
-    <div  className='div2'>
-        <Paginaction totals={300} classNames={'.div2'}/>
+    <div className="div2">
+      <Paginaction totals={300} classNames={'.div2'} />
     </div>
-  )
+  );
 }
--- src/Pagination/demo/index3.jsx
+++ src/Pagination/demo/index3.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Paginaction from '..'
+import React from 'react';
+import { Paginaction } from 'react-view-design';
 export default function index() {
   return (
-    <div  className='div3'>
-        <Paginaction totals={300} showSizeChanger={true} classNames={'.div3'}/>
+    <div className="div3">
+      <Paginaction totals={300} showSizeChanger={true} classNames={'.div3'} />
     </div>
-  )
+  );
 }
