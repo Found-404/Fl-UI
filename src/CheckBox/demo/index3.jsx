4b18484810cad553755cc29823880b8b4633a2bf
Found-404 <2247124474@qq.com>
2022-11-29 11:46:39 +0800

    fix(Checkbox): 修复checkbox组件多余依赖文件显示

--- src/CheckBox/demo/index3.jsx
+++ src/CheckBox/demo/index3.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import CheckBox from '..'
+import React from 'react';
+import { CheckBox } from 'react-view-design';
 
 export default () => {
-    return (
-        <div className='fl-checkBoxGroup-one'>
-            <CheckBox text='Apple' />
-            <CheckBox text='Orange' />
-            <CheckBox text='Strawberry' disabled />
-        </div>
-    )
-}
+  return (
+    <div className="fl-checkBoxGroup-one">
+      <CheckBox text="Apple" />
+      <CheckBox text="Orange" />
+      <CheckBox text="Strawberry" disabled />
+    </div>
+  );
+};
