a8d946ea6f7abd6d062d75f59262cf4cce2e4d63
Found-404 <2247124474@qq.com>
2022-12-07 10:37:24 +0800

    fix(Select): 修复Select组件多余文件依赖

--- src/Select/demo/index4.tsx
+++ src/Select/demo/index4.tsx
@@ -1,30 +1,26 @@
-import React from 'react'
-import Selector from '..'
+import React from 'react';
+import { Select } from 'react-view-design';
+
 export default () => {
-    let option = [
-        {
-          label: 'Mucy',
-          value: 'mucy',
-          disable:true
-        },
-        {
-          label: 'Mike',
-          value: 'mike',
-        },
-        {
-          label: 'aMck',
-          value: 'amck',
-        },
-      ]
-      
-    return (
-        <div>
-            <Selector
-                option={option}
-                placeholder={'请输入'}
-                readOnly
-            />
-        </div>
-    )
-    
-}
+  let option = [
+    {
+      label: 'Mucy',
+      value: 'mucy',
+      disable: true,
+    },
+    {
+      label: 'Mike',
+      value: 'mike',
+    },
+    {
+      label: 'aMck',
+      value: 'amck',
+    },
+  ];
+
+  return (
+    <div>
+      <Select option={option} placeholder={'请输入'} readOnly />
+    </div>
+  );
+};
